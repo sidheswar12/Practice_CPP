85d938c3d7242b32b8ebb245ad7caa60549fda46
Sidheswar Ghosh <sidheswar.ghosh@gmail.com>
2020-04-25 21:40:14 +0200

    added reserve funcgtion example

--- src/emplace.cpp
+++ src/emplace.cpp
@@ -2,18 +2,27 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int main ()
 {
-  std::vector<int> myvector = {10,20,30};
+  vector<int> myvector = {10,20,30};
   
-  auto it = myvector.emplace ( myvector.begin()+1, 100 );
-  myvector.emplace ( it, 200 );
-  myvector.emplace ( myvector.end(), 300 );
+  auto it = myvector.emplace ( myvector.begin()+1, 100 ); //push at begining+1
+  myvector.emplace ( it, 200 ); //push at index
+  myvector.emplace ( myvector.end(), 300 ); //push at end
 
-  std::cout << "myvector contains:";
+  cout << "myvector contains:";
   for (auto& x: myvector)
-    std::cout << ' ' << x;
-  std::cout << '\n';
+    cout << ' ' << x;
+  cout << endl;
+
+  myvector.emplace_back(30); //push at end
+
+  cout << "myvector contains after emplace_back:";
+  for (auto& x: myvector)
+    cout << ' ' << x;
+  cout << endl;
 
   return 0;
 }
